8d798fe38e47cdc2f9f6aa4ca59905ef93204ff8
cchura94 <cchura.cpc@gmail.com>
2023-01-03 21:44:06 -0400

    NPM

--- src/index.js
+++ src/index.js
@@ -28,8 +28,9 @@ io.on('connection', (socket) => {
 
 });
 
+const PORT = process.env.PORT || 3000;
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log('listening on *:3000');
 });
 
